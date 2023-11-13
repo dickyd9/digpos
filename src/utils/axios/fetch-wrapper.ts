42cf8bf38bee3763e66902181562894741aa4ff1
dickyd9 <dickyd6@gmail.com>
2023-11-13 21:29:29 +0700

    update

--- src/utils/axios/fetch-wrapper.ts
+++ src/utils/axios/fetch-wrapper.ts
@@ -4,7 +4,8 @@ import router from "@/router"
 import { toast } from "vue3-toastify"
 
 const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  // baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: `https://api.digvation.tech/api`,
   withCredentials: false,
   headers: {
     "Access-Control-Allow-Origin": "*",
