558274b768bc2e52c7182c2c2d9b804d1fc0b505
dickyd9 <dickyd6@gmail.com>
2023-11-20 22:11:45 +0700

    update

--- src/pages/pos/DialogPaymentUpdate.vue
+++ src/pages/pos/DialogPaymentUpdate.vue
@@ -75,8 +75,8 @@
     }
   }
 
-  import * as pdfMake from "pdfmake/build/pdfmake"
-  import * as pdfFonts from "pdfmake/build/vfs_fonts"
+  import pdfMake from "pdfmake/build/pdfmake"
+  import pdfFonts from "pdfmake/build/vfs_fonts"
 
   ;(<any>pdfMake).addVirtualFileSystem(pdfFonts)
 
